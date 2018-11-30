d5088b80c1d5e5e7906d1f515ebd83d5cd5ff223
RickVenema <31951183+RickVenema@users.noreply.github.com>
2018-11-30 16:48:00 +0100

    Parsing logic added

--- src/main/java/BioLoesJava/DB/BioLoesCNFReader.java
+++ src/main/java/BioLoesJava/DB/BioLoesCNFReader.java
@@ -2,16 +2,31 @@ package BioLoesJava.DB;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BioLoesCNFReader {
 
     public BioLoesDBOptions read(String filepath){
         if( checkcnffile(filepath)){
             List<String> records = openFile(filepath);
+            Map<String, String> parsed_cnf = new HashMap<>();
             for(String record: records){
                 System.out.println("record = " + record);
-
+                if(record.startsWith("user=")){
+                    String[] tmp = record.split("user=");
+                    parsed_cnf.put("user", tmp[1]);
+                } else if(record.startsWith("password=")){
+                    String[] tmp = record.split("password=");
+                    parsed_cnf.put("password", tmp[1]);
+                }else if(record.startsWith("database=")){
+                    String[] tmp = record.split("database=");
+                    parsed_cnf.put("database", tmp[1]);
+                }else if(record.startsWith("host=")){
+                    String[] tmp = record.split("host=");
+                    parsed_cnf.put("host", tmp[1]);
+                }
             }
         } else{
             System.err.println("Something went wrong, please read error log for more information");
